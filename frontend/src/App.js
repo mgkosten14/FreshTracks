c91ea29ec5e538216c7a38715d24d37d50868a67
mgkos <madeline.kosten@uvm.edu>
2025-05-25 17:51:31 -0600

    part of react set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,27 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [resorts, setResorts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/resorts')
+      .then(res => res.json())
+      .then(data => setResorts(data));
+  }, []);
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Ski Resorts</h1>
+      <ul>
+        {resorts.map((resort, index) => (
+          <li key={index}>
+            {resort.name} – {resort.state}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
 
