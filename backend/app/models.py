4559a64ece88b9e8ec1dda0939e86d7c3c3b262f
mgkos <madeline.kosten@uvm.edu>
2025-05-25 17:11:57 -0600

    resort class created

--- backend/app/models.py
+++ backend/app/models.py
@@ -41,3 +41,68 @@ class Person:
     def set_trip_length(self, trip_length):
         self.trip_length = trip_length
 
+# SET UP CLASS FOR RESORT
+class Resort:
+    def __init__(self, name, epic, ikon, state, country, ski_acres, base_elevation, top_elevation, trails, avg_snowfall, avg_days_snow, season_length):
+        self.name = name
+        self.epic = epic
+        self.ikon = ikon
+        self.state = state
+        self.country = country
+        self.ski_acres = ski_acres
+        self.base_elevation = base_elevation
+        self.top_elevation = top_elevation
+        self.trails = trails
+        self.avg_snowfall = avg_snowfall
+        self.avg_days_snow = avg_days_snow
+        self.season_length = season_length
+    def get_name(self):
+        return self.name
+    def get_epic(self):
+        return self.epic
+    def get_ikon(self):
+        return self.ikon
+    def get_state(self):
+        return self.state
+    def get_country(self):
+        return self.country
+    def get_ski_acres(self):
+        return self.ski_acres
+    def get_base_elevation(self):
+        return self.base_elevation
+    def get_top_elevation(self):
+        return self.top_elevation
+    def get_trails(self):
+        return self.trails
+    def get_avg_snowfall(self):
+        return self.avg_snowfall
+    def get_avg_days_snow(self):
+        return self.avg_days_snow
+    def get_season_length(self):
+        return self.season_length
+    # SETTERS
+    def set_name(self, name):
+        self.name = name
+    def set_epic(self, epic):
+        self.epic = epic
+    def set_ikon(self, ikon):
+        self.ikon = ikon
+    def set_state(self, state):
+        self.state = state
+    def set_country(self, country):
+        self.country = country
+    def set_ski_acres(self, ski_acres):
+        self.ski_acres = ski_acres
+    def set_base_elevation(self, base_elevation):
+        self.base_elevation = base_elevation
+    def set_top_elevation(self, top_elevation):
+        self.top_elevation = top_elevation
+    def set_trails(self, trails):
+        self.trails = trails
+    def set_avg_snowfall(self, avg_snowfall):
+        self.avg_snowfall = avg_snowfall
+    def set_avg_days_snow(self, avg_days_snow):
+        self.avg_days_snow = avg_days_snow
+    def set_season_length(self, season_length):
+        self.season_length = season_length
+
